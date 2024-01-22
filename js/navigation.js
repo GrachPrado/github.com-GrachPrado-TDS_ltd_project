177dcfe5c2f0000a40bab1d5e2db7cdbdc0fb33f
Bohdan Grachov <bgrachov.94@gmail.com>
2024-01-22 19:08:58 +0200

    test of new form

--- js/navigation.js
+++ js/navigation.js
@@ -7,18 +7,3 @@ function closeNav() {
 
 document.querySelector("#closeMenu").addEventListener("click", closeNav);
 document.querySelector("#openNav").addEventListener("click", openNav);
-
-
-/* Email addition */
-    // document.getElementById('myForm').addEventListener('submit', function(e) {
-    //     e.preventDefault();
-    //     var formData = new FormData(this);
-    //     fetch('https://script.google.com/macros/s/AKfycbz8opymOR3-JqcsmpyLx0nUoXNV9pDDn-ebzFx_FoXZQQqbB9gBdwnWBexCugb7IzYbMg/exec', { 
-    //         method: 'POST', 
-    //         body: formData 
-    //     }).then(response => response.text())
-    //     .then(data => alert("Повідомлення відправлено!"))
-    //     .catch(error => alert("Сталася помилка: " + error));
-    // });
-
-    
