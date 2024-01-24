cd9735b30167e8daf974174944a071da83b6d38d
Bohdan Grachov <bgrachov.94@gmail.com>
2024-01-24 15:40:39 +0200

    js added to parralax

--- js/navigation.js
+++ js/navigation.js
@@ -7,3 +7,11 @@ function closeNav() {
 
 document.querySelector("#closeMenu").addEventListener("click", closeNav);
 document.querySelector("#openNav").addEventListener("click", openNav);
+
+
+window.addEventListener('scroll', function() {
+  var scrollPosition = window.scrollY;
+  var backgroundDiv = document.querySelector('.experience__cards > div');
+  backgroundDiv.style.backgroundPositionY = scrollPosition * 0.5 + 'px';
+});
+
